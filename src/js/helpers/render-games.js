76c10938999d8f3503976d9e48bd6c2bf4ef89b2
Mahar Vladyslav <maharvladyslav@gmail.com>
2026-05-31 14:43:31 +0300

    Add game rendering functionality and controls to the main page

---
+++ src/js/helpers/render-games.js
@@ -0,0 +1,20 @@
+export const renderGames = (container, games) => {
+  if (!container) {
+    console.error("Відсутній контейнер для ігор");
+    return;
+  }
+
+  const markup = `<ul class="game-list">${games
+    .map((game) => {
+      return `<li class="game-item">
+            <article class="game">
+              <h2 class="game-name">${game}</h2>
+            </article>
+          </li>`;
+    })
+    .join("")}     
+        </ul>`;
+
+  console.log(markup);
+  container.innerHTML = markup;
+};
---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import { renderGames } from "./helpers/render-games.js";
+const games = [
+  "Starfield",
+  "Baldur's Gate 3",
+  "Cyberpunk 2077: Phantom Liberty",
+  "Hogwarts Legacy",
+  "Alan Wake 2",
+  "Spider-Man 2",
+  "Diablo IV",
+  "Final Fantasy XVI",
+  "Resident Evil 4 Remake",
+  "The Legend of Zelda: Tears of the Kingdom",
+];
+
+const gamesContainer = document.querySelector("[data-games]");
+renderGames(gamesContainer, games);
